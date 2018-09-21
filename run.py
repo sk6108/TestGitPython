bcd28182fc39b13c7836015359202060e9150cbf
sk186171 <sarah.klein@teradata.com>
2018-09-21 11:55:56 +0200

    ordering

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-#!/opt/anaconda/bin/python
+#!/opt/anaconda/bin/python3.6
 
-import re
 import sys
 #import git 
 import os
@@ -21,10 +20,10 @@ def get_input_data():
         return input_par[0]
 
 def get_files(file_name):    
-	path = "/home/beehive/gitScripts/"
-	os.system("cd /home/beehive/gitScripts/ && /opt/anaconda/bin/git pull")
-	os.system('act -U db_superuser -w db_superuser -c "\\remove' + file_name + '"')
-   	os.system('act -U db_superuser -w db_superuser -c "\install' + path + file_name + '"')
+    path = "/home/beehive/gitScripts/"
+    os.system("cd /home/beehive/gitScripts/ && /opt/anaconda/bin/git pull")
+    os.system('act -U db_superuser -w db_superuser -c "\\remove' + file_name + '"')
+    os.system('act -U db_superuser -w db_superuser -c "\install' + path + file_name + '"')
 
 
 def main():
