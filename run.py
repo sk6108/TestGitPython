7ff25d1d17bd14bfa82e4f4f102d4a6b86d81d60
sk186171 <sarah.klein@teradata.com>
2018-09-21 11:56:37 +0200

    add space

--- run.py
+++ run.py
@@ -22,8 +22,8 @@ def get_input_data():
 def get_files(file_name):    
     path = "/home/beehive/gitScripts/"
     os.system("cd /home/beehive/gitScripts/ && /opt/anaconda/bin/git pull")
-    os.system('act -U db_superuser -w db_superuser -c "\\remove' + file_name + '"')
-    os.system('act -U db_superuser -w db_superuser -c "\install' + path + file_name + '"')
+    os.system('act -U db_superuser -w db_superuser -c "\\remove ' + file_name + '"')
+    os.system('act -U db_superuser -w db_superuser -c "\install ' + path + file_name + '"')
 
 
 def main():
